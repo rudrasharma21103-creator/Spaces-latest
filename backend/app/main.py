19ec107797d30afb4117d98b8a6678f485e41bfd
Rudra Sharma <rudrasharma21103@gmail.com>
2026-01-06 11:07:22 +0530

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -28,13 +28,14 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
+        "https://spaces-lake.vercel.app"
         "http://localhost:5173",
         "http://localhost:5174",
         "http://localhost:3000",
         "http://127.0.0.1:5173",
         "http://127.0.0.1:5174",
     ],
-    allow_origin_regex=r"https://spaces-lake.vercel.app",
+    
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
